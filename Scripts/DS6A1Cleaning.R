1ae43401da892065c4df2d3944f356f151cf8e71
Luis-Arnold <60340495+Luis-Arnold@users.noreply.github.com>
2024-10-16 17:57:49 +0200

    Adding to data cleaning

--- Scripts/DS6A1Cleaning.R
+++ Scripts/DS6A1Cleaning.R
@@ -1,4 +1,7 @@
 library(readr)
 DataSetA1 <- read_delim("~/GitHub/FHDS6/Data/DataSetA1.csv", 
                         delim = ";", escape_double = FALSE, trim_ws = TRUE)
-View(DataSetA1)
+
+head(DataSetA1)
+
+plot(DataSetA1)
