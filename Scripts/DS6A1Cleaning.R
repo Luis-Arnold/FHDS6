0806224edab7daf3c55671efe11b293015b2e278
Luis-Arnold <60340495+Luis-Arnold@users.noreply.github.com>
2024-10-18 13:03:46 +0200

    Exploring Data and documenting

--- Scripts/DS6A1Cleaning.R
+++
@@ -1,7 +0,0 @@
-library(readr)
-DataSetA1 <- read_delim("~/GitHub/FHDS6/Data/DataSetA1.csv", 
-                        delim = ";", escape_double = FALSE, trim_ws = TRUE)
-
-head(DataSetA1)
-
-plot(DataSetA1)
---
+++ Scripts/DavidLuis/DS6A1Cleaning.R
@@ -0,0 +1,45 @@
+install.packages("tidyr")
+install.packages("dplyr")
+install.packages("caret")
+install.packages("randomForest")
+
+library(readr)
+library(dplyr)
+library(tidyr)
+library(caret)
+library(randomForest)
+
+Assignment_1_Dataset <- read_delim("RawData/Assignment_1-Dataset.csv", 
+                                   delim = ";", escape_double = FALSE, trim_ws = TRUE)
+
+tempData <- Assignment_1_Dataset
+
+View(tempData)
+
+head(tempData)
+
+plot(tempData)
+
+tempData <- tempData %>%
+  select(-id, -member_id, -desc, -pymnt_plan, -policy_code, -application_type, -verification_status_joint, -url) %>%
+  na.omit(tempData$int_rate)
+
+View(tempData)
+
+which(is.na(tempData$int_rate))
+
+clean <- function() {
+}
+
+set.seed(1)
+# help(topic = "createDataPartition", package = "caret")
+splitIndex <- createDataPartition(tempData$int_rate, p = .8, list = FALSE)
+trainData <- tempData[splitIndex,]
+testData <- tempData[-splitIndex,]
+
+View(trainData)
+
+help(topic = "randomForest", package = "randomForest")
+model <- randomForest(trainData, trainData$int_rate)
+
+
